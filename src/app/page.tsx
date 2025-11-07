647198805de4a7e58f034ca9d0a7630d1972cbec
ryo28 <wakisan1@ezweb.ne.jp>
2025-11-07 16:02:18 +0900

    ホームページにポートフォリオコンポーネントを追加し、"Hello, World!"の見出しを削除

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,9 @@
+import PortfolioPage from "./_components/PortfolioPage";
+
 export default function Home() {
   return (
     <div>
-      <h1>Hello, World!</h1>
+      <PortfolioPage />
     </div>
   );
 }
